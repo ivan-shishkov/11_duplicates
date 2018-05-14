8a763b9be8fd095bd170e63bb0afffe103638b37
Ivan Shishkov <mail-shishkov@yandex.ru>
2018-05-14 20:29:14 +0300

    Added getting of path from command line

--- duplicates.py
+++ duplicates.py
@@ -1,5 +1,24 @@
+import argparse
+
+
+def parse_command_line_arguments():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        'path',
+        help='a path for search duplicates of files (including subdirectories)',
+        type=str,
+    )
+
+    command_line_arguments = parser.parse_args()
+
+    return command_line_arguments
+
+
 def main():
-    pass
+    command_line_arguments = parse_command_line_arguments()
+
+    path = command_line_arguments.path
 
 
 if __name__ == '__main__':
